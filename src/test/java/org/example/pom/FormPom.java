420361dd22cd3fcc30991bc192fceb9d9ea775dc
Alexandria <alexandriaturcanu@gmail.com>
2025-10-12 14:16:56 +0300

    Initial commit

---
+++ src/test/java/org/example/pom/FormPom.java
@@ -0,0 +1,131 @@
+package org.example.pom;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.io.File;
+import java.time.Duration;
+
+import static org.example.utils.Utils.explicitWait;
+import static org.example.utils.Utils.fluentWait;
+
+public class FormPom {
+
+    private final WebDriver driver;
+    public static JavascriptExecutor js;
+
+    @FindBy(xpath = "//*[@id='firstName']")
+    private WebElement firstName;
+
+    @FindBy(xpath = "//*[@id='lastName']")
+    private WebElement lastName;
+
+    @FindBy(xpath = "//*[@id='userEmail']")
+    private WebElement userEmail;
+
+    @FindBy(xpath = "//*[@id='userNumber']")
+    private WebElement userNumber;
+
+    @FindBy(xpath = "//*[@id='subjectsInput']")
+    private WebElement subjectsInput;
+
+    @FindBy(xpath = "//*[@id='dateOfBirthInput']")
+    private WebElement dateOfBirthInput;
+
+    @FindBy(xpath = "//*[@id='state']")
+    private WebElement stateDropdown;
+
+    @FindBy(xpath = "//*[@id='city']")
+    private WebElement cityDropdown;
+
+    public void setState(String stateParam) {
+        stateDropdown.click();
+        fluentWait(driver, By.xpath("//*[text()='" + stateParam + "']"), 10);
+        WebElement state = driver.findElement(By.xpath("//*[text()='" + stateParam + "']"));
+        state.click();
+    }
+
+    public void setCity(String cityParam) {
+        cityDropdown.click();
+        fluentWait(driver, By.xpath("//*[text()='" + cityParam + "']"), 10);
+        WebElement city = driver.findElement(By.xpath("//*[text()='" + cityParam + "']"));
+        city.click();
+    }
+
+    public void setHobbies(String hobbiesParam) {
+        WebElement hobby = driver.findElement(By.xpath("//*[@id='hobbiesWrapper']//label[text()='"+ hobbiesParam +"']/../input"));
+        hobby.sendKeys(" ");
+    }
+
+    public void setSubjects(String subjectParam) {
+        subjectsInput.sendKeys(subjectParam);
+        subjectsInput.sendKeys(Keys.ENTER);
+    }
+
+    public void setGender(String genderParam) {
+        WebElement element = driver.findElement(By.xpath("//*[@id='genterWrapper']//label[text()='" + genderParam + "']"));
+        element.click();
+    }
+
+    public FormPom(WebDriver driver) {
+        this.driver = driver;
+        js =  (JavascriptExecutor) driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setLastName(String lastNameParam) {
+        lastName.clear();
+        lastName.sendKeys(lastNameParam);
+    }
+
+    public void setFirstName(String firstNameParam) {
+        firstName.clear();
+        firstName.sendKeys(firstNameParam);
+    }
+
+    public void setUserEmail(String userEmailParam) {
+        userEmail.clear();
+        userEmail.sendKeys(userEmailParam);
+    }
+
+    public void setUserNumber(String userNumberParam) {
+        userNumber.clear();
+        userNumber.sendKeys(userNumberParam);
+    }
+
+    public void setDateOfBirth(String dateOfBirthParam) {
+        dateOfBirthInput.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        dateOfBirthInput.sendKeys(dateOfBirthParam);
+        dateOfBirthInput.sendKeys(Keys.ENTER);
+
+    }
+
+    public void submitForm() {
+        // Așteaptă ca butonul să fie clicabil
+        WebElement submitBtn = explicitWait(driver, ExpectedConditions.elementToBeClickable(By.id("submit")), 10);
+        submitBtn.click();
+    }
+
+    public void closeAdvert() {
+        try {
+            js.executeScript("var elem = document.evaluate(\"//*[@id='adplus-anchor']\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;" +
+                    "elem.parentNode.removeChild(elem);");
+        } catch (Exception ignored) {}
+        try {
+            js.executeScript("var elem = document.evaluate(\"//footer\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;" +
+                    "elem.parentNode.removeChild(elem);");
+        } catch (Exception ignored) {}
+    }
+
+    public void scrollToEl() {
+        scrollToElement(dateOfBirthInput);
+    }
+
+    public void scrollToElement(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+}
---
+++ src/test/java/org/example/test/FormTest.java
@@ -0,0 +1,98 @@
+package org.example.test;
+
+import org.example.pom.FormPom;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FormTest {
+
+    public static final Logger logger =  LogManager.getLogger(FormTest.class);
+
+    static public WebDriver driver;
+    static public String URL = "https://demoqa.com/automation-practice-form";
+    static public String FIRST_NAME = "Alexandria";
+    static public String LAST_NAME = "Țurcanu";
+    static public String USER_EMAIL = "alexandriaturcanu@gmail.com";
+    static public String GENDER = "Female";
+    static public String NUMBER = "0672269811";
+    static public String SUBJECTS1 = "Maths";
+    static public String SUBJECTS2 = "English";
+    static public String DATE_OF_BIRTH = "31 Jan 2007";
+    static public String HOBBY1 = "Sports";
+    static public String HOBBY2 = "Music";
+    static public String STATE = "Haryana";
+    static public String CITY = "Panipat";
+
+    @BeforeClass
+    public void beforeTest(){
+        logger.info("Start beforeTest");
+        driver = Driver.getRemoteDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void formTest() throws InterruptedException {
+        logger.info("Start formTest");
+        driver.get(URL);
+        FormPom form = new FormPom(driver);
+        form.closeAdvert();
+        form.setFirstName(FIRST_NAME);
+        form.setLastName(LAST_NAME);
+        form.setUserEmail(USER_EMAIL);
+        form.scrollToEl();
+        form.setGender(GENDER);
+        form.setUserNumber(NUMBER);
+        form.scrollToEl();
+        form.setDateOfBirth(DATE_OF_BIRTH);
+        form.setSubjects(SUBJECTS1);
+        form.setSubjects(SUBJECTS2);
+        form.setHobbies(HOBBY1);
+        form.setHobbies(HOBBY2);
+        form.setState(STATE);
+        form.setCity(CITY);
+        form.submitForm();
+
+        Thread.sleep(3000);
+
+        WebElement actualName = driver.findElement(By.xpath("//table//*[text()='Student Name']/../td[2]"));
+        Assert.assertEquals(actualName.getText(), FIRST_NAME + " " + LAST_NAME);
+
+        WebElement actualEmail = driver.findElement(By.xpath("//table//*[text()='Student Email']/../td[2]"));
+        Assert.assertEquals(actualEmail.getText(), USER_EMAIL);
+
+        WebElement actualGender = driver.findElement(By.xpath("//table//*[text()='Gender']/../td[2]"));
+        Assert.assertEquals(actualGender.getText(), GENDER);
+
+        WebElement actualMobile = driver.findElement(By.xpath("//table//*[text()='Mobile']/../td[2]"));
+        Assert.assertEquals(actualMobile.getText(), NUMBER);
+
+        WebElement actualDateOfBirth = driver.findElement(By.xpath("//table//*[text()='Date of Birth']/../td[2]"));
+        Assert.assertEquals(actualDateOfBirth.getText(), "31 January,2007", "Date of Birth nu corespunde!");
+
+        WebElement actualSubjects = driver.findElement(By.xpath("//table//*[text()='Subjects']/../td[2]"));
+        Assert.assertEquals(actualSubjects.getText(), SUBJECTS1 + ", " + SUBJECTS2);
+
+        WebElement actualHobbies = driver.findElement(By.xpath("//table//*[text()='Hobbies']/../td[2]"));
+        Assert.assertEquals(actualHobbies.getText(), HOBBY1 + ", " + HOBBY2);
+
+        WebElement actualStateAndCity = driver.findElement(By.xpath("//table//*[text()='State and City']/../td[2]"));
+        Assert.assertEquals(actualStateAndCity.getText(), STATE + " " + CITY, "State and City nu corespunde!");
+
+        System.out.println();
+        logger.info("Finish formTest");
+    }
+    @AfterClass
+    public void afterTest(){
+
+//      driver.quit();
+        logger.info("Finish afterTest");
+
+    }
+}
